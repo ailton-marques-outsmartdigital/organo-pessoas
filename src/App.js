61dc2c2c9f0b3dc7d4e9a64de614f694869bfa79
ailton-marques-outsmartdigital <ailton.marques@outsmartdigital.com.br>
2023-01-24 18:54:17 -0300

    Adding team list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import './App.css';
 import Form from './components/Form/Form';
 import Banner from './components/Banner/Banner';
+import Team from './components/Team';
 import { useState } from 'react';
 
 function App() {
   const [members, setMembers] =useState([])
+
+  const teams = [
+    {
+    teamName: 'Programação',
+    primaryColor: '#57C278',
+    secondaryColor: '#D9F7E9'
+  },
+  {
+    teamName: 'Front End',
+    primaryColor: '#82CFFA',
+    secondaryColor: '#E8FFFF'
+  },   {
+    teamName: 'Data Science',
+    primaryColor: '#A6D157',
+    secondaryColor: '#E9FFE3'
+  },   {
+    teamName: 'Devops',
+    primaryColor: '#E06B69',
+    secondaryColor: '#FDE7E8'
+  },   {
+    teamName: 'UX e Design',
+    primaryColor: '#DB6EBF',
+    secondaryColor: '#FAE9F5'
+  },   {
+    teamName: 'Mobile',
+    primaryColor: '#FFBA05',
+    secondaryColor: '#FFF5D9'
+  },   {
+    teamName: 'Inovação e Gestão',
+    primaryColor: '#FF8A29',
+    secondaryColor: '#FFEEDF'
+  },
+];
 
   const onNewMember = (member) => {
     console.log(member);
@@ -15,6 +49,12 @@ function App() {
     <div className="App">
       <Banner />
       <Form  onMemberAdded={member => onNewMember(member)} />
+
+      {teams.map(team => <Team key={team.teamName} teamName={team.teamName} primary={team.primaryColor} secondary={team.secondaryColor} />)}
+
+      {/* <Team teamName="Front End" />
+      <Team teamName="Data Science" />
+      <Team teamName="Devops" /> */}
     </div>
   );
 }
---
+++ src/components/Team/index.js
@@ -0,0 +1,11 @@
+import './Team.css'
+
+const Team = (props) => {
+  return (
+    <section className='team'>
+      <h3>{props.teamName}</h3>
+    </section>
+  )
+}
+
+export default Team;
